f63ba944b5e508c03ae4f0bd1827b6c8c7886d50
BBS <butchbs1982@gmail.com>
2020-05-27 18:08:56 +0300

    added script php

---
+++ nitro/mail.php
@@ -0,0 +1,30 @@
+﻿<?php
+    $to = 'info@prodimp.ru';
+    $firstname = $_POST["fname"];
+    $subject= $_POST["subject"];
+    $email= $_POST["email"];
+    $text= $_POST["message"];
+    
+
+
+    $headers = 'MIME-Version: 1.0' . "\r\n";
+    $headers .= "From: " . $email . "\r\n"; // Sender's E-mail
+    $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+
+    $message ='<table style="width:100%">
+        <tr>
+            <td>'.$firstname.'  '.$laststname.'</td>
+        </tr>
+        <tr><td>Subject: '.$subject.'</td></tr>
+        <tr><td>Email: '.$email.'</td></tr>
+        <tr><td>Message: '.$text.'</td></tr>
+        
+    </table>';
+
+    if (@mail($to, $email, $message, $headers))
+    {
+        echo 'Ваше сообщение успешно отправлено.';
+    }else{
+        echo 'Ошибка отправки';
+    }
+?>
